8ddda81fba0d7d4581192804895e3ca3bc700374
Marcin <m.szablak30@gmail.com>
2024-03-15 20:10:15 +0100

    done work for today

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,17 +1,36 @@
-import './NavBar.css'
-import Nav from 'react-bootstrap/Nav'
-
+import './NavBar.css';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
 
 function NavBar() {
 
   return (
-    <Nav variant='pills' activeKey="1">
-        <Nav.Item>
-            <Nav.Link>
-                Kebab
-            </Nav.Link>
-        </Nav.Item>
-    </Nav>
+    <Navbar
+      variant='pills'
+      activeKey="1"
+      bg="primary"
+      data-bs-theme="dark"
+    >
+        <Container>
+          <Navbar.Brand href="#Home"><img
+              alt=""
+              src="/kebab-transformed.png"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+          />{' '}
+          Kebab
+          </Navbar.Brand>
+        </Container>
+        <Container>
+          <Nav>
+            <Nav.Link href="#AboutUs">About us</Nav.Link>
+            <Nav.Link href="#Menu">Menu</Nav.Link>
+            <Nav.Link href="#News">Menu</Nav.Link>
+          </Nav>
+        </Container>
+    </Navbar>
   )
 }
 
