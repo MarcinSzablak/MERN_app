e633fc3990fd11e4c22084c9ac76a820142f0de0
Marcin Szablak <m.szablak30@gmail.com>
2024-06-02 17:50:21 +0200

    Add Compatibility to react-router

--- client/src/routes/App.jsx
+++ client/src/routes/App.jsx
@@ -1,13 +1,11 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './components/NavBar'
-import TestUser from './components/TestUser';
-import NavBar from './components/NavBar';
+import NavBar from '../components/NavBar';
 //import Slider from './components/mainPage/Slider';
-import Footer from './components/Footer';
-import WelcomeImg from './components/mainPage/WelcomeImg';
-import AboutUsMainPage from './components/mainPage/AboutUsMainPage';
-import TopDishes from './components/mainPage/TopDishes';
+import Footer from '../components/Footer';
+import WelcomeImg from '../components/mainPage/WelcomeImg';
+import AboutUsMainPage from '../components/mainPage/AboutUsMainPage';
+import TopDishes from '../components/mainPage/TopDishes';
 
 function App() {
   return (
